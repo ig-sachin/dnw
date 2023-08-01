d7d8456651a568c317b79a2cbd1950896cdc7e3b
ig-sachin <sachinvishwakarma7977@gmail.com>
2023-08-01 10:35:43 +0530

    first commit

---
+++ src/main/java/com/p1/DnWApplication.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnWApplication.class, args);
+	}
+
+}
